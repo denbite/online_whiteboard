dca920fb97fd065b3832cf90da4fd62117552310
Den <mirloxd3@gmail.com>
2021-01-12 14:00:29 +0200

    connected redux, added store provider

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import { createStore } from 'redux';
+import { rootReducer } from '../store/reducers';
+import { Provider } from 'react-redux';
+
+const store = createStore(rootReducer);
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
 
 export default MyApp
