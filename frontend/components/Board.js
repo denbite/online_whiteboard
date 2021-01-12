31e9c92500176c8d24340ef866980d2dfc07cdfe
Den <mirloxd3@gmail.com>
2021-01-12 12:45:32 +0200

    added buttons for color and width switcher

--- frontend/components/Board.js
+++ frontend/components/Board.js
@@ -3,10 +3,91 @@ import React from 'react';
 import styles from '../styles/Board.module.css'
 
 export default class Board extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+          ctx: null,
+          pos: {
+              x: 0,
+              y: 0
+          }
+        };
+      }
+
+    componentDidMount = () => {
+        this.setState({
+            ctx: this.canvas.getContext('2d')
+        }, () => {
+            this.state.ctx.canvas.width = window.innerWidth;
+            this.state.ctx.canvas.height = window.innerHeight;
+
+            // init brush settings with default values
+            this.setLineWidth();
+            this.setBrushColor();
+            this.state.ctx.lineCap = 'round';
+
+            this.canvas.addEventListener('mousemove', this.draw);
+            this.canvas.addEventListener('mousedown', this.setPosition);
+            this.canvas.addEventListener('mouseenter', this.setPosition);
+        })
+    }
+
+    componentWillUnmount = () => {
+        this.setState({
+            ctx: null,
+            pos: {
+                x: 0,
+                y: 0
+            }
+          });
+
+        delete this.canvas;
+    }
+
+    setPosition = (e) => {
+        this.setState({
+            pos: {
+                x: e.clientX - e.target.offsetLeft,
+                y: e.clientY - e.target.offsetTop
+            }
+        })
+    }
+
+    setLineWidth = (width = 3) => {
+
+        if (!Number.isInteger(width)) {
+            width = 3; // set to default value
+        }
+
+        this.state.ctx.lineWidth = width;
+    }
+
+    setBrushColor = (color = "#c0392b") => {
+        if (!color instanceof String || color.length != 7){
+            color = "#c0392b"; // default color
+        }
+
+        this.state.ctx.strokeStyle = color;
+    }
+
+    draw = (e) => {
+        // mouse left button must be pressed
+        if (e.buttons !== 1) return;
+
+        this.state.ctx.beginPath(); // begin
+
+        this.state.ctx.moveTo(this.state.pos.x, this.state.pos.y); // from
+        this.setPosition(e);
+        this.state.ctx.lineTo(this.state.pos.x, this.state.pos.y); // to
+
+        this.state.ctx.stroke(); // draw it!
+    }
+
     render(){
         return (
         <div className={styles.canvasContainer}>
-                <canvas className={styles.canvas} id="drawingCanvas">
+                <canvas className={styles.canvas} id="drawingCanvas" ref={(element) => {this.canvas = element}}>
                 </canvas>
           </div>
         )
--- frontend/components/Toolbar.js
+++ frontend/components/Toolbar.js
@@ -3,16 +3,38 @@ import React from 'react';
 import styles from '../styles/Toolbar.module.css'
 
 export default class Toolbar extends React.Component{
+    saveCanvas = (event) => {
+        console.log('clicked!');
+    }
+
+    clearCanvas = (event) => {
+        console.log('cleared!');
+    }
+
     render(){
         return (
         <div className={styles.toolbar}>
-        - Операции -<br></br>
-            <button className={styles.saveButton} id="saveButton">
-            Сохранить
-            </button>
-            <button className={styles.clearButton} id="clearButton">
-            Очистить
-            </button>
+            <div className={styles.toolbarBlock}>
+            - Операции -<br/>
+                <button className={styles.saveButton} id="saveButton" onClick={this.saveCanvas}>
+                Поделиться
+                </button>
+                <button className={styles.clearButton} id="clearButton" onClick={this.clearCanvas}>
+                Очистить
+                </button>
+            </div>
+            <div className={styles.toolbarBlock}>
+            - Цвет -<br/>
+                <img className={styles.toolbarImgButton} id="redPen" src="http://professorweb.ru/downloads/pen_red.gif" alt="Красная кисть" onclick="changeColor('rgb(212,21,29)', this)"/>
+                <img className={styles.toolbarImgButton} id="greenPen" src="http://professorweb.ru/downloads/pen_green.gif" alt="Зеленая кисть" onclick="changeColor('rgb(131,190,61)', this)"/> 
+                <img className={styles.toolbarImgButton} id="bluePen" src="http://professorweb.ru/downloads/pen_blue.gif" alt="Синяя кисть" onclick="changeColor('rgb(0,86,166)', this)"/>
+            </div>
+            <div className={styles.toolbarBlock}>
+            - Толщина -<br/>
+                <img className={styles.toolbarImgButton} src="http://professorweb.ru/downloads/pen_thin.gif" alt="Тонкая кисть" onclick="changeThickness(1, this)"/>
+                <img className={styles.toolbarImgButton} src="http://professorweb.ru/downloads/pen_medium.gif" alt="Нормальная кисть" onclick="changeThickness(5, this)"/> 
+                <img className={styles.toolbarImgButton} src="http://professorweb.ru/downloads/pen_thick.gif" alt="Толстая кисть" onclick="changeThickness(10, this)"/>
+            </div>
         </div>
         )
     }
